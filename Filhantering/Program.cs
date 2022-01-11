42a1130b98aa4ac018e46c81e7c23ad93add3323
Lawe Zangena <lawza02@gmail.com>
2022-01-11 14:11:49 +0100

    added all the files

---
+++ Filhantering/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+
+namespace Filhantering
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\Filhantering\Filhantering\hello.txt";
+
+
+            using (StreamWriter sw = File.CreateText(path))
+            {
+                Console.WriteLine("Vad heter du");
+                sw.WriteLine(Console.ReadLine());
+               
+            }
+
+            using (StreamReader sr = File.OpenText(path))
+            {
+              string s;
+              s = sr.ReadLine();
+                    Console.WriteLine("tryck enter för att läsa upp texten");
+                    Console.WriteLine(Console.ReadLine()); 
+                    Console.WriteLine(s);         
+                 
+            }
+
+        }
+    }
+}
